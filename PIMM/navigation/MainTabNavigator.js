b31f86734f0479755730fe3d5f66b3e6e1b50049
piiingz <ingviun@stud.ntnu.no>
2018-10-02 12:47:35 +0200

    #3 Added dummy page for steps

--- PIMM/navigation/MainTabNavigator.js
+++ PIMM/navigation/MainTabNavigator.js
@@ -1,60 +1,71 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import React from "react";
+import { Platform } from "react-native";
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import TabBarIcon from "../components/TabBarIcon";
+import LinksScreen from "../screens/LinksScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import StepScreen from "../screens/StepScreen";
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+const StepStack = createStackNavigator({
+  Steps: StepScreen
 });
 
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
+StepStack.navigationOptions = {
+  tabBarLabel: "Steps",
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
       name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
+        Platform.OS === "ios"
+          ? `ios-walk${focused ? "" : "-outline"}`
+          : "md-walk"
       }
     />
-  ),
+  )
 };
 
 const LinksStack = createStackNavigator({
-  Links: LinksScreen,
+  Links: LinksScreen
 });
 
 LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+  tabBarLabel: "Tasks",
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
+      name={
+        Platform.OS === "ios"
+          ? `ios-checkmark-circle${focused ? "" : "-outline"}`
+          : "md-checkmark-circle"
+      }
     />
-  ),
+  )
 };
 
 const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
+  Settings: SettingsScreen
 });
 
 SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
+  tabBarLabel: "Settings",
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
+      name={
+        Platform.OS === "ios"
+          ? `ios-options${focused ? "" : "-outline"}`
+          : "md-options"
+      }
     />
-  ),
+  )
 };
 
 export default createBottomTabNavigator({
-  HomeStack,
+  StepStack,
   LinksStack,
-  SettingsStack,
+  SettingsStack
 });
