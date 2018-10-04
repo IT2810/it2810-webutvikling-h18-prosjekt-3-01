81374bf90e7bf9ba78c1df7bfc209bb31b37e2ec
adamgaidi <adamgaidi@outlook.com>
2018-10-04 13:58:09 +0200

    Added all colors to colors.js, pairprogrammed with Sigrid Kvamme

--- PIMM/constants/Colors.js
+++ PIMM/constants/Colors.js
@@ -1,14 +1,53 @@
-const tintColor = '#2f95dc';
+const primaryBlue = '#5a95ff';
+const defaultGray = 'bebebe';
+const darkGray = '#707070';
+const lightGray = '#d6d6d6';
+const taskGray = '#545454';
+
 
 export default {
-  tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
+
+    //--Color Scheme--//
+
+    //Progressbar
+    stepsBarStart: '#e29e00',
+    stepsBarHalfway: '#e2bc00',
+    stepsBarAlmostThere: '#e2d300',
+    stepsBarFinished: '#aae200',
+    stepBar: lightGray,
+    stepBarText: darkGray,
+    stepBarCheckIcon: lightGray,
+    stepBarWalkIcon: darkGray,
+
+
+    //Task
+    taskCategoryTitle: '#fff',
+    taskUnselectedDateTime: defaultGray,
+    taskTitle: darkGray,
+    taskCompleted: '#e4e4e4',
+    taskText: taskGray,
+    taskIcon: taskGray,
+    taskToggleCompleted: defaultGray,
+    taskBackground: '#efefef',
+
+    //Statistics
+    statisticsText: primaryBlue,
+    statisticsStepCount: taskGray,
+
+    //Tab bar
+    tabBarLabelDefault: darkGray,
+    tabBarLabelSelected: primaryBlue,
+    tabIconDefault: defaultGray,
+    tabIconSelected: primaryBlue,
+    tabBar: '#fff',
+
+    //Categories
+    categoryGreen: '#99cc00',
+    categoryOrange: '#ff9f00',
+    categoryBlue: '#36a4dd',
+    categoryRed: '#ff4d4e',
+    primaryBlue,
+
 };
+
+
